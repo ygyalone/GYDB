3edd5bf51fc9f22ea49e062efb744ccfefa2029b
GuangYuYang <GuangYu@hhlydeMac-mini.local>
2017-03-09 14:43:01 +0800

    添加GYDB.h

---
+++ GYDB/GYDB/GYDB/GYDB.h
@@ -0,0 +1,17 @@
+//
+//  GYDB.h
+//  GYDB
+//
+//  Created by GuangYuYang on 2017/3/9.
+//  Copyright © 2017年 YGY. All rights reserved.
+//
+
+#ifndef GYDB_h
+#define GYDB_h
+
+#import "GYDatabaseManager.h"
+#import "NSObject+GYDB.h"
+#import "GYDBCondition.h"
+#import "GYDBError.h"
+
+#endif /* GYDB_h */
--- GYDB/GYDB/ViewController.m
+++ GYDB/GYDB/ViewController.m
@@ -7,9 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "GYDatabaseManager.h"
-#import "NSObject+GYDB.h"
-#import "GYDBCondition.h"
+#import "GYDB.h"
 
 @interface Person : NSObject
 @property (nonatomic, strong) NSString *name;
