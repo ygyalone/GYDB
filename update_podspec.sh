4f7178fb2bddd94fbb3f7b2141e872e9bb61a0e9
杨广宇 <yanggy@doordu.com>
2017-07-16 01:15:50 +0800

    update podspec with tag:1.0.4

--- update_podspec.sh
+++ update_podspec.sh
@@ -1,12 +1,50 @@
 #!/bin/bash
-echo "here is old tags:"
+#push changes to remote git lib
+function gitPush() {
+    git add -A
+    git commit -m"update podspec with tag:$tag"
+    git tag $1
+    git push
+    git push --tags
+}
+
+#push new podSpec to cocoapods
+function podPush() {
+    pod trunk push *.podspec --allow-warnings --verbose --no-repo-update
+}
+
+#green color echo
+function greenEcho() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+#red color echo
+function redEcho() {
+    echo -e "\033[31m$1\033[0m"
+}
+
+
+#read tag
+greenEcho "here is old tags:"
 git tag
-echo "input a new tag:"
+greenEcho "input a new tag:"
 read tag
+
+#pod trunk命令需要先注册,所以在更新版本前先判断是否注册,该条命令不在屏幕上显示
+pod trunk me > /dev/null
+if [ $? -ne 0 ]
+then
+#{ "GuangYuYang" => "ygy9916730@163.com" }
+    redEcho "pod trunk not registered!"
+    redEcho "you should run this command before push podSpec to cocoapods:"
+    redEcho "pod trunk register example@mail.com 'authorName'"
+    exit
+fi
+
 #replace version tag
 sed -i "" "/s.version/s/=.*/= \"$tag\"/g" *.podspec
-git add -A
-git commit -m"update podspec with tag:$tag"
-git push
-pod trunk push *.podspec --allow-warnings --verbose
+
+gitPush $tag
+podPush
+
 
